8abb01c9fbd5e275597e46c1460d3f8093ff9a40
ameymahadik1997 <ameymahadik1997@gmail.com>
2021-10-31 10:03:31 +0530

    NOrma

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 
 const app  = express();
 const articles = []
+const articles2 = []
 
 
 app.get('/',(req,res)=>{
@@ -29,6 +30,23 @@ app.get('/news-main',(req,res)=>{
         res.json(articles)
     }).catch((er) => console.log(er))
 });
+
+app.get('/hot-news-main',(req,res)=>{
+    axios.get('https://www.skysports.com/football/news')
+    .then((response)=>{
+        const html2 = response.data
+        const $ = cheerio.load(html2)
+
+        $(`a:contains("hospital")`,html2).each(function (){
+            const title = $(this).text()
+            const url = $(this).attr('href')
+            articles2.push({
+                title,url
+            })
+        })
+        res.json(articles2)
+    }).catch((er) => console.log(er))
+})
 
 app.listen(PORT, ()=> {
     console.log(`Server is running in PORT number that we instantiated =  ${PORT}`)
